df74b9c0efd8a9bac2f014277e997f7307e1b47b
SuyeonShinnnn <suyeons5536@gmail.com>
2025-11-11 18:26:37 +0900

    chore: Kotlin + Gradle 프로젝트 초기 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    application
 }
 
 group = "org.example"
@@ -10,7 +11,12 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("MainKt")
 }
 
 tasks.test {
