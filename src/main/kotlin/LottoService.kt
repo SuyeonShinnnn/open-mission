88729b34327cfca8354b961989d4615fc5730421
SuyeonShinnnn <suyeons5536@gmail.com>
2025-11-15 22:55:27 +0900

    fix: 발급 방법 입력 검증 오류 수정

--- src/main/kotlin/LottoService.kt
+++ src/main/kotlin/LottoService.kt
@@ -8,8 +8,8 @@ class LottoService {
     }
 
     fun validatePurchaseType(type: Int) {
-        if(type != 1 || type != 2) {
-            throw IllegalArgumentException("[ERROR] 수동 발금은 숫자 1을, 자동 발급은 숫자 2를 입력해 주세요.");
+        if(type != 1 && type != 2) {
+            throw IllegalArgumentException("[ERROR] 수동 발급은 숫자 1을, 자동 발급은 숫자 2를 입력해 주세요.");
         }
     }
 
