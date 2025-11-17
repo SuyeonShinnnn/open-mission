2137be831e85b74af2f045c094ad1d738c5d4528
SuyeonShinnnn <suyeons5536@gmail.com>
2025-11-17 23:56:02 +0900

    fix: ErrorMessage message 필드 출력되도록 수정

--- src/main/kotlin/ExceptionHandler.kt
+++ src/main/kotlin/ExceptionHandler.kt
@@ -3,9 +3,9 @@ package org.example
 object ExceptionHandler {
     fun handleException(e: Throwable) {
         when(e) {
-            is NumberFormatException -> println("\n [ERROR] ${ErrorMessage.INVALID_INPUT}\n");
+            is NumberFormatException -> println("\n [ERROR] ${ErrorMessage.INVALID_INPUT.message}\n");
             is IllegalArgumentException -> println("\n[ERROR] ${e.message}\n" )
-            else -> println("\n[ERROR] ${ErrorMessage.UNEXPECTED_ERROR}\n")
+            else -> println("\n[ERROR] ${ErrorMessage.UNEXPECTED_ERROR.message}\n")
         }
     }
 }
