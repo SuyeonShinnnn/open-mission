65e82e3c4d1aef44bb26ed499ffae8ccd9d31402
SuyeonShinnnn <suyeons5536@gmail.com>
2025-11-22 23:32:50 +0900

    test: 로또 번호 수동 입력 단위 테스트

--- src/test/kotlin/LottoServiceTest.kt
+++ src/test/kotlin/LottoServiceTest.kt
@@ -1,4 +1,5 @@
 import org.example.ErrorMessage
+import org.example.ExceptionHandler
 import org.example.LottoService
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -49,6 +50,36 @@ class LottoServiceTest {
     fun `구매 타입이 2일 때 정상 실행`() {
         assertDoesNotThrow {
             service.validatePurchaseType(2)
+        }
+    }
+
+    @Test
+    fun `로또 번호 수동 입력 숫자 외의 입력이 들어올 때 예외 발생`() {
+        assertThrows<NumberFormatException> {
+            service.parseInput("1, a, 3, 4, 5, 6")
+        }
+    }
+
+    @Test
+    fun `로또 번호 수동 입력시 6개 미만의 숫자가 입력됐을 때 예외 발생`() {
+        val e = assertThrows<IllegalArgumentException> {
+            service.parseInput("1,2,3,4,5")
+        }
+        assertEquals(ErrorMessage.INVALID_SIZE.message, e.message)
+    }
+
+    @Test
+    fun `로또 번호 수동 입력시 1-45 숫자 범위를 벗어날 때 예외 발생`() {
+        val e = assertThrows<IllegalArgumentException> {
+            service.parseInput("1,2,3,4,5,46")
+        }
+        assertEquals(ErrorMessage.INVALID_RANGE.message, e.message)
+    }
+
+    @Test
+    fun `로또 번호 수동 입력 조건 만족시 정상 실행`() {
+        assertDoesNotThrow {
+            service.parseInput("1, 2, 3, 4, 5, 6")
         }
     }
 }
